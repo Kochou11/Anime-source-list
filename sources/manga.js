f8ec6561120e0bcc6a59a14fca540e9722d4c00f
Kochou11 <oklala702@gmail.com>
2025-02-08 20:21:49 +0545

    added new manga apps

--- sources/manga.js
+++ sources/manga.js
@@ -1,5 +1,14 @@
 const mangaData = {
          reading: [
+   {
+  name: "ComicK",
+  desc: "Huge library with diverse languages, advanced manga info, detailed filters, and minimal image compression with a modern and practical UI. NSFW requires login.",
+  tags: ["Multiple Languages", "Good UI"],
+  url: "https://comick.io/home2",
+  image: "https://comick.io/_next/image?url=%2Fstatic%2Ficons%2Funicorn-64-christmas.png&w=144&q=75"
+},
+   
+   
     {
       name: 'Mangadex',
       url: 'https://mangadex.org/',
@@ -14,13 +23,7 @@ const mangaData = {
       url: "https://mangaplus.shueisha.co.jp",
       image: "https://mangaplus.shueisha.co.jp/img/app_icon.919a13c7.png"
       },
-    {
-      name: "ComicK",
-      desc: "Huge library with diverse languages, advanced manga info, detailed filters, and minimal image compression with a modern and practical UI. NSFW requires login.",
-      tags: ["Multiple Languages", "Good UI"],
-      url: "https://comick.io/home2",
-      image: "https://comick.io/_next/image?url=%2Fstatic%2Ficons%2Funicorn-64-christmas.png&w=144&q=75"
-      }
+    
                 ],
   //manga apps resources 
   apps: [
@@ -30,6 +33,23 @@ const mangaData = {
       desc: 'Open-source manga reader for Android with extension support.',
       tags: ['Android', 'Open Source'],
       image: 'https://mihon.app/favicon-32x32.png'
+                    },
+                    {
+                      name: 'Kotatsu',
+                      url: 'https://kotatsu.app/',
+                      desc: 'Kotatsu is a free and open-source manga reader for Android with built-in online content sources.',
+                      tags: ['open source', 'Android', 'large sources'],
+                      image: 'https://kotatsu.app/favicon.ico?v=2'
+                    },
+                    {
+                      name: 'Komikku',
+                      url: 'https://github.com/komikku-app/komikku',
+                      desc: "A free and open source manga reader which is based off TachiyomiSY & Mihon/Tachiyomi.This fork has it's own new features and takes features from other forks regularly.",
+                      tags: ['Android', 'Open source', 'Fork'],
+                      image: 'https://raw.githubusercontent.com/komikku-app/komikku/master/.github/readme-images/app-icon.png',
+                      
+                      
+
                     }
                 ]
 };
