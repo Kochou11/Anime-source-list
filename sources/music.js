13f359e31809f26de141d72bd292eb42226ce7d2
Kochou11 <oklala702@gmail.com>
2025-02-16 20:02:44 +0545

    update music and novels

--- sources/music.js
+++ sources/music.js
@@ -6,6 +6,13 @@ const musicData = {
     desc: 'AnimeThemes is a simple and consistent repository of anime opening and ending themes.',
     tags: ['Music', 'OP', 'ED'],
     image: 'https://animethemes.moe/favicon-32x32.png'
+                }, 
+                {
+                  name: "Karaoke Mugen",
+                  url: "https://kara.moe/",
+                  desc: "karaoke platform that offers full video karaoke with synchronized lyrics. It features detailed song tags and filtering, provides both on-vocal and off-vocal versions, and has a large collection of songs.",
+                  tags: ["karaoke", "Large library"],
+                  image: "https://i.ibb.co/hFD6ytjj/favicon-V2.png"
                 }
                 ]
 };
--- sources/novels.js
+++ sources/novels.js
@@ -1,5 +1,12 @@
 const novelsData = {
            reading: [
+             {
+               name: "NovelBuddy",
+               desc: "Platform for reading light novels, web novels, and translated stories for free. It offers a large, regularly updated library, with a community-driven interface. Intrusive ads are minimal, making for a smooth reading experience.",
+               tags: ["Large library", "TTS", "Minimal AD"],
+               url: "https://novelbuddy.com/home",
+               image: "https://novelbuddy.com/static/common/mangabuddy-icon.png"
+             },
 
     {
       name: "Syosetu",
@@ -7,6 +14,13 @@ const novelsData = {
       tags: ["Web-novels", "Japanese", "Original"],
       url: "https://syosetu.com",
       image: "https://static.syosetu.com/view/images/narou.ico?psawph"
+},
+{
+  name: "NovelsOnline",
+  desc: "Free site where you can read and listen to thousands of translated novels. It’s user-friendly, but the intrusive ads can sometimes disrupt your experience.",
+  tags: ["ADs", "TTS", "Medium library"],
+  url: "https://novelsonline.net/",
+  image: "https://scontent.fjkr2-1.fna.fbcdn.net/v/t39.30808-1/241818077_322046539610982_4645327483002923224_n.jpg?stp=cp0_dst-jpg_s50x50_tt6&_nc_cat=109&ccb=1-7&_nc_sid=fe756c&_nc_ohc=uBKvlAMLsnsQ7kNvgEF5ubM&_nc_oc=AdigFOjfGBozsG7l0PrVXAIQNMh37unBzKncX7Ky5p11ZokAAjeS424MBPrEQKgH-e6HMF4nqkPewlRjJEDTQrdj&_nc_zt=24&_nc_ht=scontent.fjkr2-1.fna&edm=ANcbAXIEAAAA&_nc_gid=A0UScLMQCc7ZQzvEvL5fTTY&oh=00_AYCEBUPKQxyLYLMp7cTJftqnNHC5OLTyzNQnVrLkLE_zLQ&oe=67B7CC10"
 },
                 ],
   // novels apps resources 
